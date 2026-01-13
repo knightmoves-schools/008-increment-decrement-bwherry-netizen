c5574f0517385837f1140539998d5f1a8bd70667
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-13 18:16:25 +0000

    gg2

--- Counter.cs
+++ Counter.cs
@@ -1,18 +1,15 @@
-﻿
-namespace knightmoves;
-public class Counter
-{
-    public static int Visitors = 100;
-    public int PrefixIncrement()
+﻿    namespace knightmoves;
+    public class Counter
     {
+        public static int Visitors = 100;
+        public static int PrefixIncrement()
+        {
 
-        Visitors = ++Visitors;
-        Visitors = Visitors++;
-        Visitors = --Visitors;
-        Visitors = Visitors--;
+            ++Visitors;
+            Visitors++;
+            --Visitors;
+            Visitors--;
 
-
-
-       return Visitors;
+            return Visitors;
+        }
     }
-}
