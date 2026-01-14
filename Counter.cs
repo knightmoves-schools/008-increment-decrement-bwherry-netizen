fe891aa1bd964191baea411c6cf17f9d4e9996fe
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-14 10:30:52 -0600

    Update Counter.cs

--- Counter.cs
+++ Counter.cs
@@ -2,10 +2,13 @@
     public class Counter
     {
         public static int Visitors = 100;
+        
         public int PrefixIncrement = ++Visitors;
+        
         public int PostfixIncrement = Visitors++;
 
         public int prefixDecrement = --Visitors;
+        
         public int postfixDecrement = Visitors--;
 
     }
